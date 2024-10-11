edee54d368f7ad4ec459ca26e0588cd6d5472c98
Md Turin <matkhan01@yahoo.com>
2024-10-11 22:58:32 +0600

    delete command done

---
+++ Expense Tracker/Commands/DeleteCommand.cs
@@ -0,0 +1,32 @@
+﻿using Expense_Tracker.Interfaces;
+using Expense_Tracker.Services;
+using System.CommandLine;
+
+namespace Expense_Tracker.Commands;
+
+public class DeleteCommand : IArgsCommand
+{
+    public Command Execute()
+    {
+        var expenseIdOption = new Option<int>("--id", "Expense Id");
+        var deleteCommand = new Command("delete", "Delete Expense")
+        {
+            expenseIdOption
+        };
+
+        deleteCommand.SetHandler((int expenseId) =>
+        {
+            if (ExpenseManager.Instance.DeleteExpense(expenseId))
+            {
+                ExpenseManager.Instance.SaveAll();
+                Console.WriteLine("Expense deleted successfully");
+            }
+            else
+            {
+                Console.WriteLine("Expense delete failed!");
+            }
+        }, expenseIdOption);
+
+        return deleteCommand;
+    }
+}
